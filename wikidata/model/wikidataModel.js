4fcc8ed52c3321d57bd8c102965383f6315a65af
daariio92 <dariuscristobal@gmail.com>
2025-03-16 15:46:34 +0100

    Implementation and test of save game statistics

--- wikidata/model/wikidataModel.js
+++ wikidata/model/wikidataModel.js
@@ -6,7 +6,15 @@ const questionSchema = new mongoose.Schema({
     image: { type: String, required: true },
     category: { type: String, required: true },
 });
+const gameSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    correct: { type: Number, required: true },
+    wrong: { type: Number, required: true },
+    duration: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
 
 const Question = mongoose.model('questions', questionSchema);
+const Game = mongoose.model('Game', gameSchema);
 
-module.exports = { Question };
+module.exports = { Question, Game };
