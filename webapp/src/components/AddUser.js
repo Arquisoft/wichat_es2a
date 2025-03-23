6ad9c7869281fbd81b798f7b0d2f223516c21092
Natalia Blanco <UO295340@uniovi.es>
2025-03-23 15:46:28 +0100

    Added a photo to the login and signup screens. User existence is verified when logging in.

--- webapp/src/components/AddUser.js
+++ webapp/src/components/AddUser.js
@@ -1,8 +1,9 @@
 // src/components/AddUser.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Container, Typography, TextField, Button, Snackbar, Alert } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Container, Grid, Typography, TextField, Button, Snackbar, Alert } from '@mui/material';
+import { useNavigate, Link } from 'react-router-dom';
+import image from '../media/login.svg';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 
@@ -22,7 +23,7 @@ const AddUser = () => {
 
     } catch (error) {
       // Verificamos si el error tiene el mensaje
-      if (error.response && error.response.data.error){
+      if (error.response && error.response.data.error) {
         setError(error.response.data.error); //Mostrar el mensaje de error en la interfaz de usuario
       }
       setError(error.response.data.error);
@@ -34,49 +35,79 @@ const AddUser = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
-      <Typography component="h1" variant="h5">
-        Add User
-      </Typography>
+    <Container component="main" maxWidth="xl" sx={{ marginTop: 4 }}>
+      <Grid container spacing={2}>
+        {/* Columna de la izquierda: Formulario */}
+        <Grid item xs={12} md={4}>
+          {/* Mostrar el mensaje de error en la interfaz de usuario */}
+          {error && (
+            <Alert severity="error" sx={{ mt: 2 }}>
+              {error}
+            </Alert>
+          )}
 
-      {/* Formulario para agregar un usuario */}
-      <TextField
-        name="username"
-        margin="normal"
-        fullWidth
-        label="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+          <Typography component="h1" variant="h5">
+            Create your account
+          </Typography>
+          <Typography variant="body2" sx={{marginTop: 2}}>
+            Have an account? <Link to="/login" style={{ color: '#1976D2', textDecoration: 'none' }}>Log in now</Link>
+          </Typography>
 
-      <TextField
-        name="password"
-        margin="normal"
-        fullWidth
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          {/* Formulario para agregar un usuario */}
+          <TextField
+            name="username"
+            margin="normal"
+            fullWidth
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-      {/* Botón para agregar un usuario */}
-      <Button variant="contained" color="primary" onClick={addUser}>
-        Add User
-      </Button>
+          <TextField
+            name="password"
+            margin="normal"
+            fullWidth
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      {/* Mostrar el mensaje de error en la interfaz de usuario */}
-      {error && (
-        <Alert severity="error" sx={{mt:2}}>
-          {error}
-        </Alert>
-      )}
+          {/* Botón para agregar un usuario */}
+          <Button variant="contained" color="primary" onClick={addUser}>
+            Sign up
+          </Button>
 
-      {/* Snackbar para mostrar un mensaje de éxito */}
-      <Snackbar 
-        open={openSnackbar} 
-        autoHideDuration={6000} 
-        onClose={handleCloseSnackbar} 
-        message="User added successfully" />
+          {/* Snackbar para mostrar un mensaje de éxito */}
+          <Snackbar
+            open={openSnackbar}
+            autoHideDuration={6000}
+            onClose={handleCloseSnackbar}
+            message="User added successfully" />
+        </Grid>
+
+        {/* Columna de la derecha: Imagen */}
+        <Grid 
+          item
+          xs={12}
+          md={8}
+          sx={{
+            display: {xs: 'none', md: 'flex'},
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}>
+            <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+              <img 
+                src={image} 
+                alt="Login" 
+                style={{ 
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover' }} />
+            </div>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -1,9 +1,9 @@
 // src/components/Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import {useNavigate, Link} from 'react-router-dom';   // Importamos el hook useNavigate
-import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
-import { Typewriter } from "react-simple-typewriter"; // Animación de escritura
+import { useNavigate, Link } from 'react-router-dom';   // Importamos el hook useNavigate
+import { Container, Grid, Typography, TextField, Button, Snackbar, Alert } from '@mui/material';
+import image from '../media/login.svg';
 
 
 const Login = () => {
@@ -29,34 +29,37 @@ const Login = () => {
       // Realizamos la petición al servidor
       const response = await axios.post(`${apiEndpoint}/login`, { username, password });
 
-      const question = "Please, generate a greeting message for a student called " + username + " that is a student of the Software Architecture course in the University of Oviedo. Be nice and polite. Two to three sentences max.";
-      const model = "empathy"
+      // const question = "Please, generate a greeting message for a student called " + username + " that is a student of the Software Architecture course in the University of Oviedo. Be nice and polite. Two to three sentences max.";
+      // const model = "empathy"
 
-      if (apiKey==='None'){
-        setMessage("LLM API key is not set. Cannot contact the LLM.");
-      }
-      else{
-        const responseMessage = await axios.post(`${apiEndpoint}/askllm`, { question, model, apiKey })
-        setMessage(responseMessage.data.answer);
-      }
-      // Extract data from the response
-      const { createdAt: userCreatedAt } = response.data;
+      // if (apiKey === 'None') {
+      //   setMessage("LLM API key is not set. Cannot contact the LLM.");
+      // }
+      // else {
+      //   const responseMessage = await axios.post(`${apiEndpoint}/askllm`, { question, model, apiKey })
+      //   setMessage(responseMessage.data.answer);
+      // }
+      // // Extract data from the response
+      // const { createdAt: userCreatedAt } = response.data;
 
-      setCreatedAt(userCreatedAt);
+      // setCreatedAt(userCreatedAt);
       setLoginSuccess(true);
-      setOpenSnackbar(true);
+
 
       // Guardamos el usuario en el localStorage
       localStorage.setItem("user", JSON.stringify(response.data));
-
+      setOpenSnackbar(true); // Mostramos el snackbar
       // Redirigimos al usuario a la página del juego
       navigate('/game');
+      setOpenSnackbar(true); // Mostramos el snackbar
 
-     
     } catch (error) {
       // Caputramos errores de autenticación y mostramos el mensaje adecuado
-      const errorMsg = error.response?.data?.error || 'An error occurred';
-      setError(errorMsg);
+      if (error.response && error.response.data.error) {
+        setError(error.response.data.error); //Mostrar el mensaje de error en la interfaz de usuario
+      }
+      setError(error.response.data.error);
+
     } finally {
       setLoading(false);  // Desactivamos la carga
     }
@@ -68,24 +71,26 @@ const Login = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
-      {loginSuccess ? (
-        <div>
-          <Typewriter
-            words={[message]} // Pass your message as an array of strings
-            cursor
-            cursorStyle="|"
-            typeSpeed={50} // Typing speed in ms
-          />
-          <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
-            Your account was created on {new Date(createdAt).toLocaleDateString()}.
-          </Typography>
-        </div>
-      ) : (
-        <div>
+    <Container component="main" maxWidth="xl" sx={{ marginTop: 4 }}>
+      <Grid container spacing={2}>
+
+        {/* Columna de la izquierda: Formulario */}
+        <Grid item xs={12} md={4}>
+          {/* Mostrar el mensaje de error en la interfaz de usuario */}
+          {error && (
+            <Alert severity="error" sx={{ mt: 2 }}>
+              {error}
+            </Alert>
+          )}
           <Typography component="h1" variant="h5">
-            Login
+            Log in to your account
           </Typography>
+
+          <Typography variant="body2" sx={{ marginTop: 2 }}>
+            Don't have an account? <Link to="/adduser" style={{ color: '#1976D2', textDecoration: 'none' }}>Sign up</Link>
+          </Typography>
+
+          {/* Formulario para login */}
           <TextField
             margin="normal"
             fullWidth
@@ -99,20 +104,37 @@ const Login = () => {
             label="Password"
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value.toString())}
           />
           <Button variant="contained" color="primary" onClick={loginUser} disabled={loading}>
             {loading ? "Loggin in..." : "Login"}
           </Button>
-          <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} message="Login successful" />
-          {error && (
-            <Snackbar open={!!error} autoHideDuration={6000} onClose={() => setError('')} message={`Error: ${error}`} />
-          )}
-          <Typography variant="body2" sx={{marginTop: 2}}>
-            Don't have an account? <Link to="/adduser" style={{ color: '#1976D2', textDecoration: 'none' }}>Sign up</Link>
-          </Typography>
-        </div>
-      )}
+
+        </Grid>
+
+        {/* Columna de la derecha: Imagen */}
+        <Grid
+          item
+          xs={12}
+          md={8}
+          sx={{
+            display: { xs: 'none', md: 'flex' },
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}>
+          <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+            <img
+              src={image}
+              alt="Login"
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover'
+              }} />
+          </div>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
