f26eeaf4e2fbd388b411843aff95a4d03a4cce62
Natalia Blanco <UO295340@uniovi.es>
2025-03-30 21:33:29 +0200

    Add graphic for the statistics

--- webapp/src/components/Profile.jsx
+++ webapp/src/components/Profile.jsx
@@ -1,53 +1,162 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Box, Avatar, Button } from '@mui/material';
+import {
+    Container, Typography, Box, Avatar, Button, CircularProgress,
+    Paper, Grid
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, plugins } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 
 const ProfilePage = () => {
     const navigate = useNavigate();
     const [user, setUser] = useState({
         username: '',
     });
+    const [gameHistory, setGameHistory] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    // Suponiendo que la información del usuario está almacenada en el localStorage
+    const getUserId = () => {
+        try {
+            const userDataStr = window.localStorage.getItem('user');
+            if (!userDataStr) {
+                return null;
+            }
+            const userData = JSON.parse(userDataStr);
+            const parsedToken = userData?.token;
+
+            if (parsedToken) {
+                const decoded = jwtDecode(userData.token);
+                const decodedUserId = decoded?.userId;
+                if (decodedUserId) {
+                    return decodedUserId;
+                }
+            }
+            return null;
+        } catch (error) {
+            console.error("Error al recuperar userId:", error);
+            return null;
+        }
+    };
+
+    const userId = getUserId();
+
+    // Obtenemos las estadisticas del usuario
     useEffect(() => {
+        const fetchUserStats = async () => {
+            if (userId) {
+                try {
+                    const response = await fetch(`${apiEndpoint}/game/statistics?userId=${userId}`);
+                    const data = await response.json();
+                    setGameHistory(data);
+                } catch (error) {
+                    setError("Error al cargar las estadísticas del usuario.");
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+
         const userData = JSON.parse(localStorage.getItem('user'));
         if (userData) {
             setUser(userData);
+            fetchUserStats();
         } else {
-            // Si no hay datos, redirigir al login
             navigate('/login');
         }
-    }, [navigate]);
+    }, [navigate, userId]);
 
     const handleLogout = () => {
         localStorage.removeItem('user');
         navigate('/login');
     };
 
+    if (loading) {
+        return (
+            <Container maxWidth="sm">
+                <Typography variant='h6' align='center'>
+                    <CircularProgress /> Cargando tu perfil...
+                </Typography>
+            </Container>
+        )
+    }
+
+    // Datos para el gráfico de estadísticas
+    const correctAnswers = gameHistory.reduce((total, game) => total + game.correct, 0);
+    const wrongAnswers = gameHistory.reduce((total, game) => total + game.wrong, 0);
+
+    const data = {
+        labels: ['Correctas', 'Erróneas'],
+        datasets: [
+            {
+                label: 'Estadísticas de Juego',
+                data: [correctAnswers, wrongAnswers],
+                backgroundColor: ['#36A2EB', '#FF6384'],
+                hoverBackgroundColor: ['#36A2EB', '#FF6384'],
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            }
+        },
+        maintainAspectRatio: false,
+    }
+
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ textAlign: 'center', mt: 4 }}>
-                <Avatar sx={{ width: 120, height: 120, margin: 'auto' }} src={user.avatar} />
-                <Typography variant="h4" sx={{ mt: 2 }}>
-                    ¡Bienvenido/a {user.username || 'Usuario'}!
-                </Typography>
-                <Typography variant="body1" sx={{ mt: 1 }}>
-                    Aquí puedes ver y editar tu perfil.
-                </Typography>
+        <Container maxWidth="xl">
+            <Grid container spacing={4} sx={{padding: 2}}>
 
-                <Box sx={{ mt: 3 }}>
-                    <Button variant="contained" color="primary" onClick={() => navigate('/edit-profile')}>
-                        Editar Perfil
-                    </Button>
-                </Box>
+                {/* Columna izquierda: Gráfico de estadísticas */}
+                <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Box sx={{ mt: 4, width: '100%' }}>
+                        <Typography variant="h6" gutterBottom align="center">
+                            Estadísticas de Respuestas
+                        </Typography>
 
-                <Box sx={{ mt: 2 }}>
-                    <Button variant="outlined" color="error" onClick={handleLogout}>
-                        Cerrar Sesión
-                    </Button>
-                </Box>
-            </Box>
+                        <Paper sx={{ padding: 2, width: '100%', display: 'flex', justifyContent: 'center' }}>
+                            <Pie data={data} options={options} height={250}/>
+                        </Paper>
+                    </Box>
+                </Grid>
 
+                {/* Columna derecha: Información del usuario */}
+                <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Box sx={{ textAlign: 'center', mt: 4 }}>
+                        <Avatar sx={{ width: 120, height: 120, margin: 'auto' }} src={user.avatar} />
+                        <Typography variant="h4" sx={{ mt: 2 }}>
+                            ¡Bienvenido/a {user.username || 'Usuario'}!
+                        </Typography>
+                        <Typography variant="body1" sx={{ mt: 1 }}>
+                            Aquí puedes ver y editar tu perfil.
+                        </Typography>
+
+                        <Box sx={{ mt: 3 }}>
+                            <Button variant="contained" color="primary" onClick={() => navigate('/edit-profile')}>
+                                Editar Perfil
+                            </Button>
+                        </Box>
+
+                        <Box sx={{ mt: 2 }}>
+                            <Button variant="outlined" color="error" onClick={handleLogout}>
+                                Cerrar Sesión
+                            </Button>
+                        </Box>
+                    </Box>
+
+                    {error && <Typography color="error" align="center">{error}</Typography>}
+
+                </Grid>
+            </Grid>
         </Container>
     );
 };
