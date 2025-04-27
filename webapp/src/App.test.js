c03788c51884a4c058eba9e269bea951364aea96
daariio92 <dariuscristobal@gmail.com>
2025-04-27 12:50:12 +0200

    fourth try

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders welcome message', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const welcomeMessage = screen.getByText(/Welcome to the 2025 edition of the Software Architecture course/i);
   expect(welcomeMessage).toBeInTheDocument();
 });
--- webapp/src/components/Login.test.js
+++ webapp/src/components/Login.test.js
@@ -3,6 +3,7 @@ import { render, fireEvent, screen, waitFor, act } from '@testing-library/react'
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import Login from './Login';
+import { MemoryRouter } from 'react-router-dom';
 
 const mockAxios = new MockAdapter(axios);
 
@@ -12,7 +13,11 @@ describe('Login component', () => {
   });
 
   it('should log in successfully', async () => {
-    render(<Login />);
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const passwordInput = screen.getByLabelText(/Password/i);
@@ -34,7 +39,11 @@ describe('Login component', () => {
   });
 
   it('should handle error when logging in', async () => {
-    render(<Login />);
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
 
     const usernameInput = screen.getByLabelText(/Username/i);
     const passwordInput = screen.getByLabelText(/Password/i);
